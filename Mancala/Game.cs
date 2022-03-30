2b6907e8fa69dc89ee8913ea6cd59131d833b5f2
nchandl03 <nchandl@siue.edu>
2022-03-29 20:41:22 -0500

    added lastSeedInEmpty details, gameOver

--- Mancala/Game.cs
+++ Mancala/Game.cs
@@ -38,19 +38,29 @@ namespace Mancala
 
             board[topStoreIndex] = 0;
             board[bottomStoreIndex] = 0;
-            
+
             bottomTurn = true;
         }
 
         int getBoardIndex(int pit)
         {
             if (bottomTurn) { return pit; }
-            else { return bottomStoreIndex + 1 + pit;  }
+            else { return bottomStoreIndex + 1 + pit; }
         }
 
-        bool isEmpty (int pit)
+        int getAcrossIndex(int boardIndex)
         {
-            if (board[pit] == 0) {  return true; }
+            int acrossIndex;
+
+            if (bottomTurn) { acrossIndex = topStoreIndex - 1 - boardIndex; } 
+            else {  acrossIndex = bottomStoreIndex - (boardIndex % bottomStoreIndex); }
+
+            return acrossIndex;
+        }
+
+        bool isEmpty(int pit)
+        {
+            if (board[pit] == 0) { return true; }
             return false;
         }
 
@@ -75,11 +85,11 @@ namespace Mancala
         {
             if (bottomTurn && landingSpot == bottomStoreIndex)
             {
-                    return true;
+                return true;
             }
             else if (!bottomTurn && landingSpot == topStoreIndex)
             {
-                    return true;
+                return true;
             }
 
             return false;
@@ -88,16 +98,94 @@ namespace Mancala
         // special move #2
         bool lastSeedInEmpty(int landingSpot)
         {
-            if (bottomTurn && landingSpot >= 0 && landingSpot < bottomStoreIndex) 
+            if (bottomTurn && landingSpot >= 0 && landingSpot < bottomStoreIndex)
             {
                 if (isEmpty(landingSpot)) { return true; }
             }
-            
-            if (!bottomTurn && landingSpot > bottomStoreIndex && landingSpot < topStoreIndex) {
+
+            if (!bottomTurn && landingSpot > bottomStoreIndex && landingSpot < topStoreIndex)
+            {
                 if (isEmpty(landingSpot)) { return true; }
             }
 
             return false;
+        }
+
+        bool gameOver()
+        {
+            int sumMarbles = 0;
+            for (int i = bottomStoreIndex + 1; i < topStoreIndex; i++)
+            {
+                sumMarbles += board[i];
+            }
+            if (sumMarbles <= 0) { return true; }
+
+            sumMarbles = 0;
+            for (int i = 0; i < bottomStoreIndex; i++)
+            {
+                sumMarbles += board[i];
+            }
+            if (sumMarbles <= 0) { return true; }
+
+            return false;
+        }
+
+        int playMove(int chosenIndex)
+        {
+            if (!isValidMove(chosenIndex)) { return -1; }
+
+            int pit = getBoardIndex(chosenIndex);
+            int numMarbles = board[pit];
+
+            int dropIndex = (pit) % boardSize;
+
+            // add marbles into next pits, - landing spot and opp's store
+            for (int i = 0; i < numMarbles - 1; i++)
+            {
+                dropIndex = (dropIndex + 1) % boardSize;
+
+                if (bottomTurn && dropIndex == topStoreIndex) { dropIndex = (dropIndex + 1) % boardSize; }
+                else if (!bottomTurn && dropIndex == bottomStoreIndex) { dropIndex = (dropIndex + 1) % boardSize; }
+
+                board[dropIndex]++;
+            }
+            dropIndex = (dropIndex + 1) % boardSize;
+
+            // take another turn
+            if (lastSeedInStore(dropIndex))
+            {
+                board[dropIndex]++;
+
+            } 
+            // get opponent's seeds across from drop
+            else if (lastSeedInEmpty(dropIndex))
+            {
+                board[dropIndex]++;
+
+                int acrossIndex = getAcrossIndex(dropIndex);
+                int marblesToStore = board[dropIndex] + board[acrossIndex];
+
+                board[dropIndex] = 0;
+                board[acrossIndex] = 0;
+
+                if (bottomTurn) { board[bottomStoreIndex] += marblesToStore; }
+                else { board[topStoreIndex] += marblesToStore; }
+
+                bottomTurn = !bottomTurn;
+
+            } else
+            {
+                board[dropIndex]++;
+                bottomTurn = !bottomTurn;
+            }
+
+            if (gameOver())
+            {
+                // run game over protocol, check who wins
+                return 0;
+            }
+
+            return 1;
         }
 
     }
